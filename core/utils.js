2ae592d98515b3288e4db5f418a1d26cd4bafba1
ryntab <57367794+ryntab@users.noreply.github.com>
2025-04-10 19:01:53 -0400

    feat: :truck: write channel map to volume

--- core/utils.js
+++ core/utils.js
@@ -1,8 +1,16 @@
 import fs from 'fs';
 import path from 'path';
 
-export const LAST_ID_PATH = path.join(process.cwd(), 'last-id.txt');
-export const CHANNELS_PATH = path.join(process.cwd(), 'channelMap.json');
+const isDev = process.env.DEV === 'true';
+
+export const LAST_ID_PATH = isDev
+  ? path.join(process.cwd(), 'last-id.txt')
+  : '/data/last-id.txt';
+
+export const CHANNELS_PATH = isDev
+  ? path.join(process.cwd(), 'channelMap.json')
+  : '/data/channelMap.json';
+
 export const channelMap = new Map();
 
 if (fs.existsSync(CHANNELS_PATH)) {
@@ -10,7 +18,7 @@ if (fs.existsSync(CHANNELS_PATH)) {
   for (const [guildId, channelId] of Object.entries(saved)) {
     channelMap.set(guildId, channelId);
   }
-  console.log('📂 Loaded saved channelMap from file');
+  console.log(`📂 Loaded saved channelMap from ${CHANNELS_PATH}`);
 }
 
 export function proxyImage(url) {
